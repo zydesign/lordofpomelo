6c482d3983766501114b18734f9a1fc50f88d603
zydesign <zydesign@126.com>
2017-04-27 18:48:58 +0800

    Update action.js

--- game-server/app/domain/action/action.js
+++ game-server/app/domain/action/action.js
@@ -17,7 +17,8 @@ var Action = function(opts){
 /**
  * Update interface, default update will do nothing, every tick the update will be invoked
  * @api public
- *更新接口，默认更新不会执行任何操作，每次更新都会被调用/
+ *更新接口，默认更新不会执行任何操作，每次更新都会被调用
+ */
 Action.prototype.update = function(){
 };
 
