32e3802bf38a4cd9b25b7f2d23e2bef993156711
zydesign <zydesign@126.com>
2017-05-22 20:20:07 +0800

    Update scene.js

--- game-server/app/domain/area/scene.js
+++ game-server/app/domain/area/scene.js
@@ -5,6 +5,7 @@ var exp = module.exports;
 
 var area = null;
 
+//入口在app上下文，opt为(dataApi.area.findById(server.area))，area配置文件制定id的场景数据
 exp.init = function(opts){
   if(!area) {
     opts.weightMap = true;
