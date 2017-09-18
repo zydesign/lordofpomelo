6574fb2eae4f98d4c24c473d234f157ce32d7e3b
zydesign <zydesign@126.com>
2017-09-19 00:15:44 +0800

    Update playerFilter.js

--- game-server/app/servers/area/filter/playerFilter.js
+++ game-server/app/servers/area/filter/playerFilter.js
@@ -20,15 +20,18 @@ Filter.prototype.before = function(msg, session, next){
 		var route = msg.__route__;
 
 		if(route.search(/^area\.resourceHandler/i) == 0 || route.search(/enterScene$/i) >= 0){
+			//这里next传递（null，msg）
 			next();
 			return;
 		}else{
+			//而这里next传递（error，msg）
 			next(new Error('No player exist!'));
 			return;
 		}
 	}
 
 	if(player.died){
+		//同上
 		next(new Error("You can't move a dead man!!!"));
 		return;
 	}
