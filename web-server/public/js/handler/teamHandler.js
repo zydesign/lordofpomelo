022d9ec654d918b438a2024e5d9ab40a691c492e
zydesign <zydesign@126.com>
2017-10-02 00:56:33 +0800

    Update teamHandler.js

--- web-server/public/js/handler/teamHandler.js
+++ web-server/public/js/handler/teamHandler.js
@@ -4,6 +4,8 @@ __resources__["/teamHandler.js"] = {meta: {mimetype: "application/javascript"}, 
 
   /**
    * Execute player action
+   * 执行玩家组队动作：创建队伍、离开队伍、解散队伍
+   * 用notify只向服务器广播，无需返回响应；服务器也是通过push的形式广播给各个玩家
    */
   function exec(type, params) {
     switch (type) {
