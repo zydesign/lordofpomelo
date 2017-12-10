06c09998854660fc75b814f78920ce2f8e774bf6
zydesign <zydesign@126.com>
2017-12-10 18:56:47 +0800

    Update ai.js

--- game-server/app/ai/ai.js
+++ game-server/app/ai/ai.js
@@ -4,10 +4,15 @@ var fs = require('fs');
 var path = require('path');
 
 var exp = module.exports;
+//此脚本只要由area脚本引用，并创建AiManager
 
+//创建一个AiManager实例，主要是给参数添加大脑服务brainService实例属性，注册大脑到brainService
 exp.createManager = function(opts) {
-	var brainService = new BrainService();
+	var brainService = new BrainService();  //实例大脑服务
+	// 遍历brain目录下的所有脚本
 	fs.readdirSync(__dirname + '/brain').forEach(function(filename){
+		//解析：/.../x,表示x文件夹里，查找某文件，反斜杠\表示查找内容
+		//这里是在test文件夹里面查找带js脚本的文件
 		if (!/\.js$/.test(filename)) {
 			return;
 		}
@@ -17,6 +22,7 @@ exp.createManager = function(opts) {
 	});
 
 	opts = opts || {};
+	//给参数添加属性，大脑服务
 	opts.brainService = brainService;
 	return new AiManager(opts);
 };
