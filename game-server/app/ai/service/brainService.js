9616478b1d1a1dab74d2775519525432e2ee88e9
zydesign <zydesign@126.com>
2017-12-11 21:46:01 +0800

    Update brainService.js

--- game-server/app/ai/service/brainService.js
+++ game-server/app/ai/service/brainService.js
@@ -6,18 +6,22 @@ module.exports = Service;
 
 var pro = Service.prototype;
 
+//大脑服务可以获取大脑和注册大脑
 pro.getBrain = function(type, blackboard) {
 	//TODO: mock data
+	// 如果类型既不是autoFight，也不是player，才会执行type = 'tiger'
 	if(type !== 'autoFight' && type !== 'player') {
 		type = 'tiger';
 	}
 	var brain = this.brains[type];
+	//如果大脑组里面存在要找的大脑，就会返回一个该类型大脑实例，如果没有返回null
 	if(brain) {
 		return brain.clone({blackboard: blackboard});
 	}
 	return null;
 };
 
+ //注册大脑就是把大脑存入大脑组中
 pro.registerBrain = function(type, brain) {
 	this.brains[type] = brain;
 };
