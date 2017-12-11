4ffb30ca8c66ab8c646408f4039b5853621648c2
zydesign <zydesign@126.com>
2017-12-11 18:18:23 +0800

    Update revive.js

--- game-server/app/domain/action/revive.js
+++ game-server/app/domain/action/revive.js
@@ -3,18 +3,19 @@ var messageService = require('../messageService');
 var util = require('util');
 
 
-//复活
+//复活（怪物死亡复活）
 var Revive = function(opts){
 	opts.type = 'revive';
 	opts.id = opts.entity.entityId;
-	opts.singleton = true;
+	opts.singleton = true;   //复活是独立行为
 
+	 //上面是修改opt参数，下面是让revive继承action函数对象。结合util.inherits()继承action的原型链,
 	Action.call(this, opts);
 	this.entity = opts.entity;
 	this.area = this.entity.area;
 	this.map = opts.map;
-	this.time = opts.reviveTime;
-	this.now = Date.now();
+	this.time = opts.reviveTime;  //复活最小时间间隔
+	this.now = Date.now();   //上一次刷新时间
 };
 
 util.inherits(Revive, Action);
@@ -24,9 +25,11 @@ util.inherits(Revive, Action);
  * @api public
  */
 Revive.prototype.update = function(){
-	var time = Date.now();
+	var time = Date.now();   //当前刷新时间
 
-	this.time -= time - this.now;
+	this.time -= time - this.now;  //复活最小时间间隔与当前复活时间差，如果小于零，则可以复活
+	
+	//条件设置为小于等于10才可以复活，更新对象坐标，更新观察者，并aoi广播消息给附近的观察者，对象复活位置及状态
 	if(this.time <= 10){
 		this.entity.died = false;
 		this.entity.hp = this.entity.maxHp/2;
@@ -60,6 +63,8 @@ Revive.prototype.update = function(){
 		this.finished = true;
 		this.entity.updateTeamMemberInfo();
 	}
+	
+	//储存当前刷新时间
 	this.now = time;
 };
 
