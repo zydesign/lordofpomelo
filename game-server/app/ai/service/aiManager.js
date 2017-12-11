7e00f9b76ac96c1829ec20ba2f35739bba04df10
zydesign <zydesign@126.com>
2017-12-11 22:58:33 +0800

    Update aiManager.js

--- game-server/app/ai/service/aiManager.js
+++ game-server/app/ai/service/aiManager.js
@@ -2,6 +2,9 @@ var Blackboard = require('../meta/blackboard');
 
 var exp = module.exports;
 
+//该模块作用：1.添加角色大脑   2.删除角色大脑   3.刷新大脑信息（执行所有大脑的udate，顺序执行攻击、拾取、npc对话）
+
+//ai管理器属性有：大脑服务、场景、玩家、怪物
 var Manager = function(opts) {
 	this.brainService = opts.brainService;
 	this.area = opts.area;
@@ -26,11 +29,17 @@ pro.stop = function() {
  * Add a brain to the character if the type is mob.
  * Start the tick if it has not started yet.
  */
+
+//通过角色参数，创建大脑实例，并储存到玩家大脑组和怪物大脑中
+
+        //参数cs为角色数组
 pro.addCharacters = function(cs) {
+	//如果还没开启ai管理器或已经结束ai管理器，则不增加角色大脑
 	if(!this.started || this.closed) {
 		return;
 	}
 
+	//如果参数角色不存在则返回
 	if(!cs || !cs.length) {
 		return;
 	}
@@ -41,11 +50,15 @@ pro.addCharacters = function(cs) {
 	for(var i=0, l=cs.length; i<l; i++) {
 		c = cs[i];
 		var brain;
+		 
 		if(c.type === 'player') {
+			//如果角色组已经有了该角色，遍历下一个
 			if(this.players[c.entityId]) {
+				//continue是结束本次循环，也即是进行下一个遍历
 				continue;
 			}
 
+			//实例一个带参数的大脑，然后添加到角色组
 			brain = this.brainService.getBrain('player', Blackboard.create({
 				manager: this,
 				area: this.area,
@@ -53,10 +66,13 @@ pro.addCharacters = function(cs) {
 			}));
 			this.players[c.entityId] = brain;
 		} else {
+			//如果怪物组已经有了该怪物角色，遍历下一个
 			if(this.mobs[c.entityId]) {
 				continue;
 			}
 
+			//实例一个带参数的大脑，然后添加到怪物组
+			//无论c.characterName取什么名字，大脑服务都将非玩家归结为tiger
 			brain = this.brainService.getBrain(c.characterName, Blackboard.create({
 				manager: this,
 				area: this.area,
@@ -70,6 +86,8 @@ pro.addCharacters = function(cs) {
 /**
  * remove a character by id from ai manager
  */
+
+//从大脑组中移除某个大脑实例
 pro.removeCharacter = function(id) {
 	if(!this.started || this.closed) {
 		return;
@@ -83,6 +101,8 @@ pro.removeCharacter = function(id) {
  * Update all the managed characters.
  * Stop the tick if there is no ai mobs.
  */
+
+//更新所有大脑实例的update
 pro.update = function() {
 	if(!this.started || this.closed) {
 		return;
