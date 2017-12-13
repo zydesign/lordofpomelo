0e58771d492c69915bbb3d5f92c2421e1755b261
zydesign <zydesign@126.com>
2017-12-13 20:58:21 +0800

    Update tryAndAdjust.js

--- game-server/app/ai/node/tryAndAdjust.js
+++ game-server/app/ai/node/tryAndAdjust.js
@@ -4,6 +4,7 @@ var Sequence = bt.Sequence;
 var Select = bt.Select;
 var util = require('util');
 
+//尝试攻击并调整攻击节点，是一个选择节点
 /**
  * Try and adjust action.
  * Try to do a action and return success if the action success.
@@ -14,13 +15,18 @@ var util = require('util');
  *				opts.adjustAction {BTNode} adjust action
  *				opts.tryAction {BTNode} try action}
  */
+
+
+//opt参数提供属性：blackboard、tryAction、adjustAction（MoveToTarget）
 var Node = function(opts) {
 	BTNode.call(this, opts.blackboard);
 
+	//调整再攻击（顺序节点）
 	var adjustAndTryAgain = new Sequence(opts);
 	adjustAndTryAgain.addChild(opts.adjustAction);
 	adjustAndTryAgain.addChild(opts.tryAction);
 
+	//主节点，，，尝试攻击并调整再攻击（选择节点）
 	var tryAndAdjust = new Select(opts);
 	tryAndAdjust.addChild(opts.tryAction);
 	tryAndAdjust.addChild(adjustAndTryAgain);
