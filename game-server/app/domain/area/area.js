94c60222fae39ce6698364ddb207f93af54b4c0a
zydesign <zydesign@126.com>
2017-12-16 23:08:40 +0800

    Update area.js

--- game-server/app/domain/area/area.js
+++ game-server/app/domain/area/area.js
@@ -28,11 +28,11 @@ var Instance = function(opts){
   this.map = opts.map;  //参数提供的地图实例。opt.map这个属性是scene另外添加的
 
   //The map from player to entity
-  this.players = {};
-  this.users = {};
-  this.entities = {};   //这里储存角色、怪物、道具等实体
+  this.players = {};  //储存角色的实体id组
+  this.users = {};    //储存角色id组
+  this.entities = {};   //这里储存角色、怪物、道具等实体，是所有类型的实体
   this.zones = {};
-  this.items = {};
+  this.items = {};   //储存道具和装备的实体id组
   this.channel = null;
 
   this.playerNum = 0;
@@ -131,48 +131,64 @@ Instance.prototype.initMobZones = function(mobZones) {
  * Add entity to area
  * @param {Object} e Entity to add to the area.
  */
+
+//场景添加实体，加入实体组，加入事件管理器，加入场景频道channel，加入ai大脑，加入aoi观察者等等，添加成功会返回true=========
 Instance.prototype.addEntity = function(e) {
   var entities = this.entities;
   var players = this.players;
   var users = this.users;
 
+  //判断实体是否存在
   if(!e || !e.entityId) {
     return false;
   }
 
+  //判断是否已经添加过该角色
   if(!!players[e.id]) {
     logger.error('add player twice! player : %j', e);
     return false;
   }
 
+  
   //Set area and areaId
+  //给参数加入场景属性
   e.area = this;
 
+  //加入实体组
   entities[e.entityId] = e;
+  //加入事件
   eventManager.addEvent(e);
 
+  //如果实体类型为玩家，加入频道channel，加入ai大脑，加入aoi观察者，加入角色id组
   if(e.type === EntityType.PLAYER) {
     this.getChannel().add(e.userId, e.serverId);
     this.aiManager.addCharacters([e]);
 
     this.aoi.addWatcher({id: e.entityId, type: e.type}, {x : e.x, y: e.y}, e.range);
-    players[e.id] = e.entityId;
-    users[e.userId] = e.id;
+    players[e.id] = e.entityId;   //该角色的实体id
+    users[e.userId] = e.id;   //对应用户的角色id（player.id）
 
-    this.playerNum++;
+    this.playerNum++;   //记录场景有多少玩家
     utils.myPrint('e = ', JSON.stringify(e));
     utils.myPrint('e.teamId = ', JSON.stringify(e.teamId));
     utils.myPrint('e.isCaptain = ', JSON.stringify(e.isCaptain));
+    
+   //如果实体类型为怪物
   }else if(e.type === EntityType.MOB) {
     this.aiManager.addCharacters([e]);
 
     this.aoi.addWatcher({id: e.entityId, type: e.type}, {x : e.x, y: e.y}, e.range);
+    
+    //如果实体类型为道具
   }else if(e.type === EntityType.ITEM) {
     this.items[e.entityId] = e.entityId;
+    
+    //如果实体类型为装备
   }else if(e.type === EntityType.EQUIPMENT) {
     this.items[e.entityId] = e.entityId;
   }
 
+  //无论实体什么类型，要加入实体对象的aoi，以便观察者知道其位置
   this.aoi.addObject({id:e.entityId, type:e.type}, {x: e.x, y: e.y});
   return true;
 };
@@ -182,6 +198,8 @@ Instance.prototype.addEntity = function(e) {
  * @param {Number} entityId The entityId to remove
  * @return {boolean} remove result
  */
+
+//场景中删除实体，包括从频道中移除，从ai大脑组中移除，从巡逻组中移除，从aoi对象组中移除，停止该实体的动作等，删除成功返回true========
 Instance.prototype.removeEntity = function(entityId) {
   var zones = this.zones;
   var entities = this.entities;
@@ -254,6 +272,7 @@ Instance.prototype.removeEntity = function(entityId) {
  * Get entity from area
  * @param {Number} entityId.
  */
+//从场景中获取实体
 Instance.prototype.getEntity = function(entityId) {
   var entity = this.entities[entityId];
   if (!entity) {
@@ -267,13 +286,15 @@ Instance.prototype.getEntity = function(entityId) {
  * @param {Array} The given entities' list.
  * @return {Map} The entities
  */
+//从场景中获取一批实体，返回对象
 Instance.prototype.getEntities = function(ids) {
   var result = {};
 
-  result.length = 0;
+  result.length = 0;  //记录实体的数量，也就是ids的数量
   for(var i = 0; i < ids.length; i++) {
     var entity = this.entities[ids[i]];
     if(!!entity) {
+      //不同类型放到不同数组中
       if(!result[entity.type]){
         result[entity.type] = [];
       }
@@ -286,8 +307,9 @@ Instance.prototype.getEntities = function(ids) {
   return result;
 };
 
+//获取一批角色，返回数组
 Instance.prototype.getAllPlayers = function() {
-  var _players = [];
+  var _players = []; 
   for(var id in this.players) {
     _players.push(this.entities[this.players[id]]);
   }
@@ -295,13 +317,17 @@ Instance.prototype.getAllPlayers = function() {
   return _players;
 };
 
+//获取所有实体，返回场景实体组
 Instance.prototype.getAllEntities = function() {
   return this.entities;
 };
 
+//获取单个角色，返回该角色的实体，如果没有返回null
 Instance.prototype.getPlayer = function(playerId) {
+  //先获取该角色的实体id
   var entityId = this.players[playerId];
 
+  //然后从实体组中获取对应的实体
   if(!!entityId) {
     return this.entities[entityId];
   }
@@ -309,6 +335,7 @@ Instance.prototype.getPlayer = function(playerId) {
   return null;
 };
 
+//通过角色id，删除场景中的单个角色实体
 Instance.prototype.removePlayer = function(playerId) {
   var entityId = this.players[playerId];
 
@@ -317,6 +344,7 @@ Instance.prototype.removePlayer = function(playerId) {
   }
 };
 
+//通过uid，删除场景中的单个角色实体，这里要用户组中多删一个角色id
 Instance.prototype.removePlayerByUid = function(uid){
   var users = this.users;
   var playerId = users[uid];
@@ -332,6 +360,7 @@ Instance.prototype.removePlayerByUid = function(uid){
  * @param {Object} pos Given position, like {10,20}.
  * @param {Number} range The range of the view, is the circle radius.
  */
+//获取一个灯塔范围内的实体
 Instance.prototype.getAreaInfo = function(pos, range) {
   var ids = this.aoi.getIdsByPos(pos, range);
   return this.getEntities(ids);
@@ -343,6 +372,7 @@ Instance.prototype.getAreaInfo = function(pos, range) {
  * @param {Array} types The types of the object need to find.
  * @param {Number} range The range of the view, is the circle radius.
  */
+//通过位置、类型、、范围，获取一批实体，不同类型为一个数组，返回一个result对象
 Instance.prototype.getEntitiesByPos = function(pos, types, range) {
   var entities = this.entities;
   var idsMap = this.aoi.getIdsByRange(pos, range, types);
@@ -364,6 +394,7 @@ Instance.prototype.getEntitiesByPos = function(pos, types, range) {
   return result;
 };
 
+//判断场景是否有玩家
 Instance.prototype.isEmpty = function(){
   return this.playerNum === 0;
 };
