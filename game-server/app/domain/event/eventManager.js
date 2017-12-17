68bcd114bc7c4a853e31ca28f535059d04b13d73
zydesign <zydesign@126.com>
2017-12-18 01:14:50 +0800

    Update eventManager.js

--- game-server/app/domain/event/eventManager.js
+++ game-server/app/domain/event/eventManager.js
@@ -34,9 +34,11 @@ exp.addEvent = function(entity){
 //其中背包、装备继承基类Persistent而调用save()函数。
 
 //持久化同步的作用：激活储存事件，会先储存数据到内存队列中，同步模块sync会间隔一定时间将内存数据保存到数据库
+//pomelo-sync的api:sync.exec(key,id,val,cb)---key为连接数据库的增删改查函数，id为要查询的表的第id条数据，val为提供给key的sql的值
 function addSaveEvent(player) {
 	var app = pomelo.app;
 	player.on('save', function() {
+		//'playerSync.updatePlayer'数据库连接查询函数，player.id查询的id位置，player.strip()为提供的sql的值
 		app.get('sync').exec('playerSync.updatePlayer', player.id, player.strip());
 	});
 
