baf1cdbbffc4d6ab9a17097485a4514324ee8dce
zydesign <zydesign@126.com>
2017-12-17 19:31:32 +0800

    Update eventManager.js

--- game-server/app/domain/event/eventManager.js
+++ game-server/app/domain/event/eventManager.js
@@ -9,6 +9,7 @@ var exp = module.exports;
 /**
  * Listen event for entity
  */
+//area场景添加实体时，添加事件。（何时添加实体？area初始化NPCs时，timer更新怪物空间时，怪物掉落道具时，玩家进入场景时）
 exp.addEvent = function(entity){
 	switch(entity.type){
 		case EntityType.PLAYER :
@@ -29,6 +30,8 @@ exp.addEvent = function(entity){
  * Add save event for player
  * @param {Object} player The player to add save event for.
  */
+//实体类型为玩家时，存数据事件。角色、背包、装备三个实体监听了'save'事件.有各自的save（）函数包裹着发射事件。
+//其中背包、装备继承基类Persistent而调用save()函数。
 function addSaveEvent(player) {
 	var app = pomelo.app;
 	player.on('save', function() {
