f4e7d4df1d617ad7c72e3c818706617b7541a423
zydesign <zydesign@126.com>
2017-12-17 21:06:27 +0800

    Update bag.js

--- game-server/app/domain/bag.js
+++ game-server/app/domain/bag.js
@@ -13,7 +13,7 @@ var logger = require('pomelo-logger').getLogger(__filename);
  */
 var Bag = function(opts) {
   Persistent.call(this, opts);
-  this.itemCount = opts.itemCount || 20;
+  this.itemCount = opts.itemCount || 20;  //背包道具栏数量
   this.items = opts.items || {};
 };
 
@@ -51,14 +51,17 @@ Bag.prototype.getData = function() {
  * @return {number}
  * @api public
  */
+//ai大脑player执行拾取函数pickItem（e）时，执行这个背包添加道具函数-商城购买道具也会执行该函数-------------------------------
 Bag.prototype.addItem = function(item) {
   var index = -1;
 
+  //条件有一个成立，就返回index
   if (!item || !item.id || !item.type || !item.type.match(/item|equipment/)) {
     return index;
   }
 
   for (var i = 1; i <= this.itemCount; i ++) {
+    //如果某个道具栏为空，该道具栏填充道具，标签标记道具的位置，跳出遍历
     if (!this.items[i]) {
       this.items[i] = {id: item.id, type: item.type};
       index = i;
@@ -66,10 +69,12 @@ Bag.prototype.addItem = function(item) {
     }
   }
 
+  //如果标签>0，执行背包储存函数，发射save事件，同步数据到数据库
   if (index > 0) {
     this.save();
   }
 
+  //返回储存的位置（第一格为1，第二个为2，以此类推...）
   return index;
 };
 
