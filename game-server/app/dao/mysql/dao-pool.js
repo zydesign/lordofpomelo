e9ce7c48555311cd116958c24b1809e4307649c7
zydesign <zydesign@126.com>
2017-12-18 00:07:45 +0800

    Update dao-pool.js

--- game-server/app/dao/mysql/dao-pool.js
+++ game-server/app/dao/mysql/dao-pool.js
@@ -4,13 +4,13 @@ var mysql = require('mysql');
 /*
  * Create mysql connection pool.
  */
+//创建数据库连接池，注意作用是使用账号、密码创建数据库连接，以便增删改查数据.............................
 var createMysqlPool = function(app){
-	//从app对象中获取数据库配置
-	var mysqlConfig = app.get('mysql');
+	var mysqlConfig = app.get('mysql');  //从app对象中获取数据库配置（连接数据库的账号、密码）
 	const factory = {
 		create: function(){
 			return new Promise(function(resolve, reject){
-				//通过数据库模块创建一个连接到数据库
+				//通过数据库模块mysql，创建一个连接到数据库
 				var client = mysql.createConnection({
 					host: mysqlConfig.host,
 					user: mysqlConfig.user,
