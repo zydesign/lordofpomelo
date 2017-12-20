c3be449cf9241ca4c6dde649db6968d617f3e477
zydesign <zydesign@126.com>
2017-12-20 23:04:39 +0800

    Update equipments.js

--- game-server/app/domain/equipments.js
+++ game-server/app/domain/equipments.js
@@ -46,11 +46,13 @@ var dict = {
   '戒指': 'ring'
 };
 
-//转换类型
+//转换类型，返回装备类型type
 var convertType = function (type) {
+//test用于检测字符串是否匹配某个模式，[\u4e00-\u9fa5]匹配中文，这里意思就是检测type是否为中文，如果是中文，获取它的英文标签
   if (/[\u4e00-\u9fa5]/.test(type)) {
     type = dict[type];
   } else {
+	  //如果检测到type不是中文，则type转为小写英文标签。【toLowerCase() 方法用于把字符串转换为小写】
     type = type.toLowerCase();
   }
 
@@ -58,23 +60,28 @@ var convertType = function (type) {
 };
 
 //Get equipment by type
+//获取装备部位 
 Equipments.prototype.get = function(type) {
   return this[convertType(type)];
 };
 
 //Equip equipment by type and id
+//某类型部位装备某id的装备，并同步数据库
 Equipments.prototype.equip = function(type, id) {
   this[convertType(type)] = id;
   this.save();
 };
 
 //Unequip equipment by type
+//某类型部位装备归零，并同步到数据库
 Equipments.prototype.unEquip = function(type) {
   this[convertType(type)] = 0;
   this.save();
 };
 
+//装备清单，Underscore下划线模块，给dict的值前面加上下划线
 var EquipList = Underscore.values(dict);
+//判断是否着装了某装备
 Equipments.prototype.isEquipment = function(strEquip) {
   return Underscore.contains(EquipList, strEquip);
 };
