f6e511dda8fe70158943489bf13623a0e34cedc8
zydesign <zydesign@126.com>
2017-12-21 23:43:28 +0800

    Update executeTask.js

--- game-server/app/domain/executeTask.js
+++ game-server/app/domain/executeTask.js
@@ -11,6 +11,7 @@ var async = require('async');
 /**
  * Expose 'executeTask'.
  */
+//执行任务，玩家接受任务后，开始执行任务刷怪，收集道具等，更新任务完成进度，推送给客户端
 var executeTask = module.exports;
 
 /**
@@ -22,24 +23,29 @@ var executeTask = module.exports;
  * @param {Character} killed, the killed character(mob/player)
  * @api public
  */
+//击杀怪物后，调用该函数
 executeTask.updateTaskData = function(player, killed) {
+	//先排除角色类型是怪物
   if (player.type === consts.EntityType.MOB) {return;}
 	var tasks = player.curTasks;
 	var reData = null;
 	for (var id in tasks) {
 		var task = tasks[id];
+		//如果无任务或任务完成或任务完成一半，则遍历下一个任务
 		if (typeof task === 'undefined' || task.taskState >= consts.TaskState.COMPLETED_NOT_DELIVERY)	{
 			continue;
 		}
+		//任务描述切割为数组
 		var taskDesc = task.desc.split(';');
-		var taskType = task.type;
-		var killedNum = task.completeCondition[taskDesc[1]];
+		var taskType = task.type;    //任务类型
+		var killedNum = task.completeCondition[taskDesc[1]];  //刷怪数量
+		//满足3个条件：任务类型为击杀怪物，被击杀的目标是怪物，被击杀的目标类型id是描述的第二项
 		if (taskType === consts.TaskType.KILL_MOB && killed.type === consts.EntityType.MOB && killed.kindId === parseInt(taskDesc[1])) {
-			task.taskData.mobKilled += 1;
+			task.taskData.mobKilled += 1;  //该属性由Player.startTask函数提供
 			reData = reData || {};
 			reData[id] = task.taskData;
-			task.save();
-			player.curTasks[id] = task;
+			task.save();   //更新了taskData后，同步数据库
+			player.curTasks[id] = task;  //更新角色player的当前任务
 			if (player.curTasks[id].taskData.mobKilled >= killedNum) {
 				isCompleted(player, id);
 			}
