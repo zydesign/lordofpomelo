20274c90bc3a66958f1d8ca12fe743de8858c0e7
zydesign <zydesign@126.com>
2017-12-24 00:28:27 +0800

    Update mobzone.js

--- game-server/app/domain/map/mobzone.js
+++ game-server/app/domain/map/mobzone.js
@@ -10,7 +10,7 @@ var defaultLimit = 10;
 /**
  * The mob zone for generate mobs
  */
-//怪物空间类。在特定区域内生成怪物
+//怪物空间类。在指定场景某区域内生成怪物
 var MobZone = function(opts) {
 	Zone.call(this, opts);
 	this.area = opts.area;
@@ -19,23 +19,23 @@ var MobZone = function(opts) {
 	//通过指定的怪物id复制一份这个怪物的数据对象（单份数据的属性名为英文项目名）
 	this.mobData = utils.clone(dataApi.character.findById(this.mobId));
 	
-        //给这个怪物数据，添加数据和修改部分数据值
-	this.mobData.zoneId = this.zoneId;
-	this.mobData.areaId = this.area.id;
-	this.mobData.area = this.area;
-	this.mobData.kindId = this.mobData.id;
-	this.mobData.kindName = this.mobData.name;
-	this.mobData.level = opts.level || 1;
-	this.mobData.weaponLevel = opts.weaponLevel || 1;
-	this.mobData.armorLevel = opts.armorLevel || 1;
+        //给这个怪物数据，添加属性
+	this.mobData.zoneId = this.zoneId;                           //怪物空间所在的空间id
+	this.mobData.areaId = this.area.id;                          //所在的场景id
+	this.mobData.area = this.area;                               //所在的场景
+	this.mobData.kindId = this.mobData.id;                       //怪物类型id，即character怪物表的id
+	this.mobData.kindName = this.mobData.name;                   //怪物名字
+	this.mobData.level = opts.level || 1;                        //怪物等级
+	this.mobData.weaponLevel = opts.weaponLevel || 1;            //怪物武器等级
+	this.mobData.armorLevel = opts.armorLevel || 1;              //怪物防御等级
 
-	this.limit = opts.mobNum||defaultLimit;  //怪物数量限制
-	this.count = 0;  //当前怪物数量
-	this.mobs = {};  //怪物对象集合
+	this.limit = opts.mobNum||defaultLimit;                      //怪物数量限制
+	this.count = 0;                                              //当前怪物数量
+	this.mobs = {};                                              //怪物组
 
 	this.lastGenTime = 0;   //上一次生成怪物时间
 	this.genCount = 3;      //一次update最多可以生成3个怪物
-	this.interval = 5000;   //生成时间间隔
+	this.interval = 5000;   //生成怪物时间间隔
 };
 
 util.inherits(MobZone, Zone);
@@ -43,22 +43,25 @@ util.inherits(MobZone, Zone);
 /**
  * Every tick the update will be called to generate new mobs
  * 每一次更新都会被调用来生成新的mobs
- * 每隔5秒生成一个怪物，如果怪物达到限定数则不再生成
+ * 每隔5秒生成1~3个怪物，一般只能生成一个，如果怪物达到限定数则不再生成
  */
 
-//间隔5秒，在限制数量内在场景area中生成一个怪物实体
+//间隔5秒，在限制数量内在场景area中生成一个怪物实体。场景开启后，这个函数会被Timer定时器定时执行
 MobZone.prototype.update = function() {
 	var time = Date.now();   //当前时间
-	var nextTime = this.lastGenTime + this.interval;  //下一次生成怪物时间
+	var nextTime = this.lastGenTime + this.interval;  //生成怪物的时间
 
+	//生成怪物1~3个怪物
 	for(var i = 0; i < this.genCount; i++) {
+		//如果怪物空间怪物数量小于限制数量，而且到了怪物生成时间
 		if(this.count < this.limit && nextTime <= time) {
 			//生成一个怪物，这个怪物会被加入MobZone的怪物对象集，并生成怪物巡逻的坐标
 			this.generateMobs();  
-			this.lastGenTime = time;
+			this.lastGenTime = time;    //更新上一次怪物生成时间
 		}
 	}
 
+	//如果数量达到限制，即使没有生成怪物，也要更新上一次怪物生成时间为当前时间
 	if(this.count === this.limit) {
 		this.lastGenTime = time;
 	}
@@ -68,33 +71,35 @@ MobZone.prototype.update = function() {
  * The nenerate mob funtion, will generate mob, update aoi and push the message to all interest clients
  * 生成怪物，更新aoi并且推送消息给观察者
  */
-MobZone.prototype.generateMobs = function() {
-	//生成一份怪物数据
+MobZone.prototype.generateMobs = function() {	 
 	var mobData = this.mobData;
 	if(!mobData) {
 		logger.error('load mobData failed! mobId : ' + this.mobId);
 		return;
 	}
-        //do...while循环，如果生成的怪物数据坐标map地图内无效（不在地图内或在障碍物上），
-	//就从新生成，限制20次内，得到的是一个坐标
+        //do...while循环，如果生成的怪物数据坐标map地图内无效（不在地图内或在障碍物上），就从新生成坐标，限制20次内 	
 	var count = 0, limit = 20;
 	do{
-		//在怪物空间范围内随机生成怪物坐标
+		//怪物数据添加一个坐标属性，空间的坐标是空间方块的左下角，这里生成的坐标在方块里面
 		mobData.x = Math.floor(Math.random()*this.width) + this.x;
 		mobData.y = Math.floor(Math.random()*this.height) + this.y;
 	} while(!this.map.isReachable(mobData.x, mobData.y) && count++ < limit);
 
+	//如果随机了20次都遇到障碍物，直接返回
 	if(count > limit){
 		logger.error('generate mob failed! mob data : %j, area : %j, retry %j times', mobData, this.area.id, count);
 		return;
 	}
             
-	//生成怪物  怪物坐标  怪物巡逻路径
+	//生成怪物实例
 	var mob = new Mob(mobData);
-	mob.spawnX = mob.x;   //其实也是mobData.x
-	mob.spawnY = mob.y;   //其实也是mobData.y
-	genPatrolPath(mob);  //生成怪物巡逻路径
-	//将怪物加入到怪物空间
+	mob.spawnX = mob.x;   //给怪物添加卵坐标属性
+	mob.spawnY = mob.y; 
+	
+	//执行生成怪物巡逻路径
+	genPatrolPath(mob);
+	
+	//执行将怪物实体加入怪物组
 	this.add(mob);
         
 	//将怪物加入场景
@@ -115,6 +120,7 @@ MobZone.prototype.add = function(mob) {
  * 从怪物空间中删除怪物
  * @param {Number} id The entity id of the mob to remove.
  */
+//怪物空间的怪物组中删除指定实体id的怪物实体，返回true
 MobZone.prototype.remove = function(id) {
 	if(!!this.mobs[id]) {
 		delete this.mobs[id];
@@ -124,14 +130,14 @@ MobZone.prototype.remove = function(id) {
 };
 
 var PATH_LENGTH = 3;   //路径长度，坐标数量
-var MAX_PATH_COST = 300;  //
+var MAX_PATH_COST = 300;  //路径最大数量
 
 /**
  * Generate patrol path for mob
  * 生成怪物巡逻路径，返回坐标数组（4个坐标）
  */
 var genPatrolPath = function(mob) {
-	var map = mob.area.map;
+	var map = mob.area.map;  //地图
 	var path = []; //创建一个路径坐标数组
 	var x = mob.x, y = mob.y, p;
 	for(var i=0; i<PATH_LENGTH; i++) {
