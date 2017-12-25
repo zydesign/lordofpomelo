59aa987fa325cd53183d27d8bc2bd3a3b2617ee1
zydesign <zydesign@126.com>
2017-12-25 13:13:54 +0800

    Update loop.js

--- game-server/app/patrol/mode/loop.js
+++ game-server/app/patrol/mode/loop.js
@@ -9,45 +9,56 @@ var patrol = require('../patrol');
  *        opts.rounds {Number} loop rounds. -1 stands for infinite loop
  *        opts.standTick {Number} rest tick after per step. 0 stands for no rest
  */
+//巡逻系统的循环动作
 var Mode = function(opts) {
-  this.character = opts.character;
-  this.path = opts.path.slice(0);
-  this.rounds = opts.rounds || 1;
-  this.step = this.path.length;
-  this.standTick = opts.standTick || 0;
+  this.character = opts.character;          //角色实体
+  this.path = opts.path.slice(0);           //去掉角色自身坐标，剩余3个巡逻坐标
+  this.rounds = opts.rounds || 1;           //循环圈数
+  this.step = this.path.length;             //步数，坐标数量
+  this.standTick = opts.standTick || 0;     //停留时间
   this.tick = this.standTick;
-  this.started = false;
+  this.started = false;                     //是否开始
 };
 
 module.exports = Mode;
 
 var pro = Mode.prototype;
 
+
+
+//patrolManager.update就是遍历巡逻动作组，执行每一对象的update
 pro.update = function() {
+  //如果路径无坐标或循环次数为0，返回完成，不再执行后面，等待下一次update
   if(this.path.length === 0 || this.rounds === 0) {
     //if path is empty or rounds is 0
     return patrol.RES_FINISH;
   }
 
+  //如果循环动作未开启
   if(!this.started) {
     this.started = true;
+    //角色移动到第一个巡逻坐标，返回等待，不再执行后面，等待下一次update
     this.character.move(this.path[0].x, this.path[0].y, true);
     return patrol.RES_WAIT;
   }
 
   var dest = this.path[0];
+  //如果角色还没到达第一个巡逻坐标，返回等待，不再执行后面，等待下一次update
   if(this.character.x !== dest.x || this.character.y !== dest.y) {
     //if i am on the road to dest
     return patrol.RES_WAIT;
   }
 
+  //停留时间大于0，减1，返回等待，不再执行后面，等待下一次update
   if(this.tick > 0) {
     //well, we have finished a step and we can have a rest if necessary
     this.tick--;
     return patrol.RES_WAIT;
   }
+  //停留时间归零后，重置停留时间，继续执行后面
   this.tick = this.standTick;
 
+  //如果巡逻圈数大于0，步数减1（如果减1后，步数step为0了，圈数减1；如果圈数rounds也为0了，返回完成，不再执行后面，等待下一次update）
   if(this.rounds > 0) {
     //if we should count the steps and rounds
     this.step--;
@@ -56,15 +67,17 @@ pro.update = function() {
       if(this.rounds === 0) {
         return patrol.RES_FINISH;
       }
+      //如果圈数还有，重置步数step，并执行后面
       this.step = this.path.length;
     }
   }
-
+   //如果去掉原始坐标的路径长度大于1，将走过的那个坐标放到末尾..................
   if(this.path.length > 1) {
     this.path.push(this.path.shift());
   }
 
   //move to next destination
+  //然后角色移动到下一个坐标，返回等待
   this.character.move(this.path[0].x, this.path[0].y, true);
   return patrol.RES_WAIT;
 };
