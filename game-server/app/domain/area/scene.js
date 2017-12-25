01c67563c38ce45ed84f97a53f3015d88cf8f479
zydesign <zydesign@126.com>
2017-12-25 23:12:26 +0800

    Update scene.js

--- game-server/app/domain/area/scene.js
+++ game-server/app/domain/area/scene.js
@@ -5,7 +5,7 @@ var exp = module.exports;
 
 var area = null;
 
-//场景area的入口，init（opt）由app提供参数启动，并且opt属性添加2项
+//场景area的入口，init（opt）由app提供参数启动，并且给参数opt提供地图map属性，地图权重weightMap属性 
 exp.init = function(opts){
   if(!area) {
     opts.weightMap = true;
