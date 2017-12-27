93acc0933bcfe67c8fddb865eab74430662e5c2c
zydesign <zydesign@126.com>
2017-12-27 20:02:01 +0800

    Update composite.js

--- game-server/app/patrol/mode/composite.js
+++ game-server/app/patrol/mode/composite.js
@@ -1,5 +1,6 @@
 var patrol = require('../patrol');
 
+//复合模式：添加子巡逻模式组，
 /**
  * Composite mode: compose children and invoke them one by one.
  */
@@ -16,7 +17,11 @@ pro.add = function(mode) {
   this.children.push(mode);
 };
 
+
+//这个update是半闭包。一个一个执行子模式的update，有等待则等待，则退出闭包等下一次刷新，直到子模式返回完成，才会执行下一个子模式
+//（只有子模式返回等待，退出闭包等下一次刷新，或者子模式全部完成也退出闭包）
 pro.update = function() {
+  //如果所有子模式的update都返回RES_FINISH，就会index===length，就会返回成功，退出闭包
   if(this.index >= this.children.length) {
     return patrol.RES_FINISH;
   }
