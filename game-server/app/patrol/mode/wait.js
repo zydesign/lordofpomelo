d43a50232051d7c1614bb35eb1e26e4cffe9fd6c
zydesign <zydesign@126.com>
2017-12-27 20:34:17 +0800

    Update wait.js

--- game-server/app/patrol/mode/wait.js
+++ game-server/app/patrol/mode/wait.js
@@ -3,6 +3,7 @@ var patrol = require('../patrol');
 /**
  * Wait mode: wait ticks and then return finish.
  */
+//等待模式。opt提供等待时间，每update一次tick减1，返回等待下一次update，
 var Mode = function(opts) {
   this.tick = opts.tick||1;
 };
