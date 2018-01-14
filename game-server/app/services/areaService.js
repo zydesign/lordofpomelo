b57b21a3f6caa2ec9c637d5e926715b4e9f59c31
zydesign <zydesign@126.com>
2018-01-15 00:35:01 +0800

    Update areaService.js

--- game-server/app/services/areaService.js
+++ game-server/app/services/areaService.js
@@ -9,15 +9,15 @@ var async = require('async');
 
 var logger = require('pomelo-logger').getLogger(__filename);
 
-var maps = {};
+var maps = {};  //游戏地图组
 
 var exp = module.exports;
 
-//入口为app，服务器开启即执行了这里的init，设置场景权重，为每个场景配置tiled maps地图，Key为场景id
+//场景服务初始化。设置场景权重，为每个场景游戏地图map，Key为场景id（app启动立即调用该函数）
 exp.init = function(){
-  var areas = dataApi.area.all(); //dataApi由app提供
+  var areas = dataApi.area.all(); //获取场景数据数组
 
-  //Init areas
+  //遍历场景数据组，设置权重，添加游戏地图
   for(var key in areas){
     //init map
     var area = areas[key];
@@ -31,7 +31,7 @@ exp.init = function(){
  * Proxy for map, get born place for given map
  * @api public
  */
-//加入tiled maps地图后，就可以通过场景id，获取tiled maps地图的  出生地
+//生成出生地。通过场景id，对应的游戏地图，获取出生地数据
 exp.getBornPlace = function(sceneId){
   return maps[sceneId].getBornPlace();
 };
@@ -40,7 +40,7 @@ exp.getBornPlace = function(sceneId){
  * Proxy for map, get born point for given map
  * @api public
  */
-//通过场景id获取  tiled maps地图的 出生点
+//获取出生点。通过场景id，对应的游戏地图，从出生地中生成出生点
 exp.getBornPoint = function(sceneId){
   return maps[sceneId].getBornPoint();
 };
@@ -51,7 +51,7 @@ exp.getBornPoint = function(sceneId){
  * @param cb {funciton} Call back funciton
  * @api public
  */
-//切换场景，更新session
+//切换场景。（playerHandler.changeArea调用该函数，客户端发起切换场景使用）
 exp.changeArea = function(args, session, cb) {
   var app = pomelo.app;
   var area = session.area;
@@ -59,23 +59,25 @@ exp.changeArea = function(args, session, cb) {
   var playerId = args.playerId;
   var target = args.target;
   var player = area.getPlayer(playerId);
-  var frontendId = args.frontendId;
+  var frontendId = args.frontendId;   
 
-  //要切换的场景 对应的场景数据
+  //通过目标areaId，获取要切换的目标场景数据
   var targetInfo = dataApi.area.findById(target);
 
+  //如果目标场景为普通场景。当前场景删除玩家，获取目标场景出生点，玩家属性直线目标场景---------------------------------普通场景
   if(targetInfo.type === AreaType.SCENE){
     area.removePlayer(playerId);
 
     var pos = this.getBornPoint(target); //获取目标场景出生点
 
-    player.areaId = target;  //修改角色场景id为目标场景id
+    player.areaId = target;  //角色场景id为目标场景id
     player.isInTeamInstance = false;
-    player.instanceId = 0;
+    player.instanceId = 0;    //玩家副本id为0，无副本
     player.x = pos.x;
     player.y = pos.y;
     utils.myPrint("1 ~ player.teamId = ", player.teamId);
-    userDao.updatePlayer(player, function(err, success) {   //将更新的角色信息同步到数据库
+    //将玩家信息player同步到数据库。重置部分session
+    userDao.updatePlayer(player, function(err, success) {   
       if(err || !success) {
         err = err || 'update player failed!';
         utils.invokeCallback(cb, err);
@@ -96,21 +98,25 @@ exp.changeArea = function(args, session, cb) {
       }
     });
   }else{
-    var closure = this;
+    //如果目标场景是单人副本或组队副本-----------------------------------------------------------------单人副本或组队副本
+    var closure = this;   //闭包
+    //异步串行（顺序执行数组函数，将每一个函数结果存入数组中）
     async.series([
       function(callback){
         //Construct params
-        var params = {areaId : args.target};
+        var params = {areaId : args.target};  //用于创建组队副本的参数params
         params.id = playerId;
 
+        //如果目标场景类型为组队副本，而且玩家队伍id存在（即玩家进入组队副本）----------------组队副本多一个params.id
         if(targetInfo.type === AreaType.TEAM_INSTANCE && player.teamId){
-          params.id = player.teamId;
+          params.id = player.teamId;  //参数加入一个玩家队伍id
         }
 
         utils.myPrint('params.id, player.teamId = ', params.id, player.teamId);
         utils.myPrint('playerId = ', player.id);
-        player.isInTeamInstance = true;
+        player.isInTeamInstance = true;  //玩家在组队副本true
         //Get target instance
+        //rpc到副本脚本，生成目标场景（副本），并重置部分session
         app.rpc.manager.instanceRemote.create(session, params, function(err, result){
           if(err){
             logger.error('get Instance error!');
@@ -122,11 +128,13 @@ exp.changeArea = function(args, session, cb) {
             session.set('isCaptain', player.isCaptain);
             session.set('isInTeamInstance', player.isInTeamInstance);
             session.pushAll();
-            player.instanceId = result.instanceId;
+            player.instanceId = result.instanceId;   //玩家的副本id
             utils.myPrint('player.instanceId = ', player.instanceId);
 
+            //如果玩家是队长，能获取到玩家队伍id，而且目标场景类型为组队副本
             if (player.isCaptain && player.teamId && targetInfo.type === AreaType.TEAM_INSTANCE) {
               utils.myPrint('DragMember2gameCopy is running ...');
+              //rpc到组队脚本，
               app.rpc.manager.teamRemote.dragMember2gameCopy(null, {teamId: player.teamId, target: target},
                 function(err, ret) {
                   if (!!err) {
@@ -158,10 +166,10 @@ exp.changeArea = function(args, session, cb) {
     ],
       function(err, result){
         if(!!err){
-          utils.invokeCallback(cb, err);
+          utils.invokeCallback(cb, err);      //如果发生错误，cb的值为err
           logger.warn('change area failed! args: %j', args);
         }else{
-          utils.invokeCallback(cb, null);
+          utils.invokeCallback(cb, null);     //如果没错误，cb的值为null
         }
       }
     );
