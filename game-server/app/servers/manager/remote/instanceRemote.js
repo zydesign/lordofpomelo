d924f4235620840d103783d3361b1fdfe18a2650
zydesign <zydesign@126.com>
2018-01-15 14:43:34 +0800

    Update instanceRemote.js

--- game-server/app/servers/manager/remote/instanceRemote.js
+++ game-server/app/servers/manager/remote/instanceRemote.js
@@ -1,13 +1,13 @@
 var utils = require('../../../util/utils');
 var instanceManager = require('../../../services/instanceManager');
 var exp = module.exports;
-//管理服务器的副本rpc
+//副本服务器rpc
 var logger = require('pomelo-logger').getLogger(__filename);
 
-//创建副本（服务areaService.changeArea调用该函数）
+//创建副本服务器（服务areaService.changeArea调用该函数）
 exp.create = function(params, cb){
   logger.error('create server params : %j', params);
-  //通过副本管理服务，生产副本实例
+  //通过副本管理服务，生成副本实例
   instanceManager.getInstance(params, function(err, result){
     if(err){
       logger.error('create instance error! args : %j, err : %j', params, err);
@@ -18,7 +18,7 @@ exp.create = function(params, cb){
   });
 };
 
-//删除副本
+//删除副本，返回被删除的
 exp.remove = function(id, cb){
   //通过副本管理服务，删除副本
   instanceManager.remove(id);
