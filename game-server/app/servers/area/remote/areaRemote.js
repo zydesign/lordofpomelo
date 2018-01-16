2a19eee1d5b0903a6d48fc0c9a538bc88c849e5a
zydesign <zydesign@126.com>
2018-01-16 12:03:11 +0800

    Update areaRemote.js

--- game-server/app/servers/area/remote/areaRemote.js
+++ game-server/app/servers/area/remote/areaRemote.js
@@ -6,7 +6,7 @@ var logger = require('pomelo-logger').getLogger(__filename);
 var exp = module.exports;
 
 // 实例场景副本（副本管理服务的instanceManager.getInstance调用该函数）
-//参数params：   {areaId : args.areaId, idinstanceId : instanceId} 目标场景id和副本id（副本id格式：1_1）
+//参数params：   {areaId : args.areaId, instanceId : instanceId} 目标场景id和副本id（副本id格式：1_1）
 exp.create = function(params, cb){
   var start = Date.now();
   var result = instancePool.create(params);
