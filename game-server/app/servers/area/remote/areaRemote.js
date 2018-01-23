c3ee0932d8b35171a62ec177fd93d42cd1297fb0
zydesign <zydesign@126.com>
2018-01-23 21:32:21 +0800

    Update areaRemote.js

--- game-server/app/servers/area/remote/areaRemote.js
+++ game-server/app/servers/area/remote/areaRemote.js
@@ -7,12 +7,12 @@ var exp = module.exports;
 
 //玩家切换场景，rpc到manager服务器，在生成副本服务器id作为路由，rpc到这里生成副本------------------------------------生成场景副本
 // 生成副本（副本管理服务的instanceManager.getInstance调用该函数）
-//参数params：   {areaId : args.areaId, instanceId : instanceId} 目标场景id和副本id（副本id格式：1_1）
+//参数params：{areaId : args.areaId, instanceId : instanceId} 目标场景id和副本id（副本id格式：1_1）
 exp.create = function(params, cb){
   var start = Date.now();
   var result = instancePool.create(params);
   var end = Date.now();
-  logger.info('create instance use time : %j', end - start);
+  logger.info('create instance use time : %j', end - start);  //打印创建副本使用时间
 
   utils.invokeCallback(cb, null, result);
 };
