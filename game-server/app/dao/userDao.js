0839d662bd91002c4653f0f45d811c2333d0ca8a
zydesign <zydesign@126.com>
2018-01-29 02:07:15 +0800

    Update userDao.js

--- game-server/app/dao/userDao.js
+++ game-server/app/dao/userDao.js
@@ -12,7 +12,7 @@ var async = require('async');
 var utils = require('../util/utils');
 var consts = require('../consts/consts');
 
-//用户数据传输
+//用户数据库传输。（PS：增insert、删delete、改update 返回对象； 查select 返回数组）
 var userDao = module.exports;
 
 /**
@@ -22,8 +22,9 @@ var userDao = module.exports;
  * @param {String} passwd
  * @param {function} cb
  */
+//通过用户名username，获取用户信息。-------------------------------------------------------------【通过username获取user用户信息】
 userDao.getUserInfo = function (username, passwd, cb) {
-	var sql = 'select * from	User where name = ?';
+	var sql = 'select * from User where name = ?';
 	var args = [username];
 
 	pomelo.app.get('dbclient').query(sql,args,function(err, res) {
@@ -35,9 +36,9 @@ userDao.getUserInfo = function (username, passwd, cb) {
 				var rs = res[0];
 				userId = rs.id;
 				rs.uid = rs.id;
-				utils.invokeCallback(cb,null, rs);
+				utils.invokeCallback(cb,null, rs);    //如果有数据，返回res[0]
 			} else {
-				utils.invokeCallback(cb, null, {uid:0, username: username});
+				utils.invokeCallback(cb, null, {uid:0, username: username});  //如果没数据，uid设置为0
 			}
 		}
 	});
@@ -45,10 +46,10 @@ userDao.getUserInfo = function (username, passwd, cb) {
 
 /**
  * Get an user's all players by userId
- * 通过uid获取所有角色信息
  * @param {Number} uid User Id.
  * @param {function} cb Callback function.
  */
+//通过uid获取玩家数据组--------------------------------------------------------------------【通过uid获取players玩家数据组】
 userDao.getPlayersByUid = function(uid, cb){
 	var sql = 'select * from Player where userId = ?';
 	var args = [uid];
@@ -59,10 +60,12 @@ userDao.getPlayersByUid = function(uid, cb){
 			return;
 		}
 
+		//如果返回数组为空数组，cb为[]
 		if(!res || res.length <= 0) {
 			utils.invokeCallback(cb, null, []);
 			return;
 		} else {
+		//如果返回结果有玩家数据，cb为数据库返回的res
 			utils.invokeCallback(cb, null, res);
 		}
 	});
@@ -74,6 +77,7 @@ userDao.getPlayersByUid = function(uid, cb){
  * @param {Number} playerId
  * @param {function} cb Callback function.
  */
+//通过playerId获取玩家数据，返回cb是玩家实体-------------------------------------------------------【通过playerId获取player实体】
 userDao.getPlayer = function(playerId, cb){
 	var sql = 'select * from Player where id = ?';
 	var args = [playerId];
@@ -81,10 +85,12 @@ userDao.getPlayer = function(playerId, cb){
 	pomelo.app.get('dbclient').query(sql,args,function(err, res){
 		if(err !== null){
 			utils.invokeCallback(cb, err.message, null);
+			//如果数据库返回空数组，cb为[]
 		} else if (!res || res.length <= 0){
 			utils.invokeCallback(cb,null,[]);
 			return;
 		} else{
+			//如果数据库返回有玩家数据，cb为res[0]
 			utils.invokeCallback(cb,null, new Player(res[0]));
 		}
 	});
@@ -96,6 +102,7 @@ userDao.getPlayer = function(playerId, cb){
  * @param {String} name Player name
  * @param {function} cb Callback function
  */
+//通过角色名name获取玩家数据-------------------------------------------------------------------------【通过角色名获取player实体】
 userDao.getPlayerByName = function(name, cb){
 	var sql = 'select * from Player where name = ?';
 	var args = [name];
@@ -103,9 +110,11 @@ userDao.getPlayerByName = function(name, cb){
 	pomelo.app.get('dbclient').query(sql,args,function(err, res){
 		if (err !== null){
 			utils.invokeCallback(cb, err.message, null);
+			//如果数据库返回空数组，cb为null
 		} else if (!res || res.length <= 0){
 			utils.invokeCallback(cb, null, null);
 		} else{
+			//如果数据库返回有玩家信息，cb为res[0]
 			utils.invokeCallback(cb,null, new Player(res[0]));
 		}
 	});
@@ -117,14 +126,17 @@ userDao.getPlayerByName = function(name, cb){
  * @param {String} playerId
  * @param {function} cb
  */
+
+//获取玩家所有数据-------------------------------------------------------------------------------【通过playerId获取PlayerAllInfo】
 userDao.getPlayerAllInfo = function (playerId, cb) {
+	//并行执行，各个函数的结果传给最后的results数组中
 	async.parallel([
 		function(callback){
 			userDao.getPlayer(playerId, function(err, player) {
 				if(!!err || !player) {
 					logger.error('Get user for userDao failed! ' + err.stack);
 				}
-				callback(err,player);
+				callback(err,player);  //得到玩家数据player
 			});
 		},
 		function(callback) {
@@ -132,7 +144,7 @@ userDao.getPlayerAllInfo = function (playerId, cb) {
 				if(!!err || !equipments) {
 					logger.error('Get equipments for eqipmentDao failed! ' + err.stack);
 				}
-				callback(err,equipments);
+				callback(err,equipments);  //得到装备数据equipments
 			});
 		},
 		function(callback) {
@@ -140,7 +152,7 @@ userDao.getPlayerAllInfo = function (playerId, cb) {
 				if(!!err || !bag) {
 					logger.error('Get bag for bagDao failed! ' + err.stack);
 				}
-				callback(err,bag);
+				callback(err,bag);  //得到背包信息bag
 			});
 		},
 		function(callback) {
@@ -148,7 +160,7 @@ userDao.getPlayerAllInfo = function (playerId, cb) {
 				if(!!err || !fightSkills){
 					logger.error('Get skills for skillDao failed! ' + err.stack);
 				}
-				callback(err, fightSkills);
+				callback(err, fightSkills);  //得到战斗技能信息fightSkills
 			});
 		},
 		function(callback){
@@ -156,11 +168,11 @@ userDao.getPlayerAllInfo = function (playerId, cb) {
 				if(!!err) {
 					logger.error('Get task for taskDao failed!');
 				}
-				callback(err, tasks);
+				callback(err, tasks);    //得到任务信息tasks
 			});
 		}
 	], 
-	function(err, results) {
+	function(err, results) {   //背包、装备、技能、任务，都存入玩家数据player中
 		var player = results[0];
 		var equipments = results[1];
 		var bag = results[2];
@@ -174,7 +186,7 @@ userDao.getPlayerAllInfo = function (playerId, cb) {
 		if (!!err){
 			utils.invokeCallback(cb,err);
 		}else{
-			utils.invokeCallback(cb,null,player);
+			utils.invokeCallback(cb,null,player);   //最后的cb传送处理过的 player数据
 		}
 	});
 };
@@ -185,15 +197,17 @@ userDao.getPlayerAllInfo = function (playerId, cb) {
  * @param {String} username
  * @param {function} cb
  */
+//通过用户名获取用户数据，cb生成user实例----------------------------------------------------------------【通过username获取user实例】
 userDao.getUserByName = function (username, cb){
-	var sql = 'select * from	User where name = ?';
+	var sql = 'select * from User where name = ?';
 	var args = [username];
 	pomelo.app.get('dbclient').query(sql,args,function(err, res){
 		if(err !== null){
 			utils.invokeCallback(cb, err.message, null);
 		} else {
 			if (!!res && res.length === 1) {
-				var rs = res[0];
+				var rs = res[0];       //从数据组[user]中获取对象user数据
+				//生成user实例
 				var user = new User({id: rs.id, name: rs.name, password: rs.password, from: rs.from});
 				utils.invokeCallback(cb, null, user);
 			} else {
@@ -209,8 +223,9 @@ userDao.getUserByName = function (username, cb){
  * @param {String} uid UserId
  * @param {function} cb Callback function
  */
+//通过uid获取user实例------------------------------------------------------------------------------------【通过uid获取user实例】
 userDao.getUserById = function (uid, cb){
-	var sql = 'select * from	User where id = ?';
+	var sql = 'select * from User where id = ?';
 	var args = [uid];
 	pomelo.app.get('dbclient').query(sql,args,function(err, res){
 		if(err !== null){
@@ -218,9 +233,11 @@ userDao.getUserById = function (uid, cb){
 			return;
 		}
 
+		//如果数据库返回数组有值，cb为生成User实例
 		if (!!res && res.length > 0) {
 			utils.invokeCallback(cb, null, new User(res[0]));
 		} else {
+		//如果数据库返回空数组，cb为null
 			utils.invokeCallback(cb, ' user not exist ', null);
 		}
 	});
@@ -232,6 +249,7 @@ userDao.getUserById = function (uid, cb){
  * @param {String} username
  * @param {function} cb Call back function.
  */
+// 删除指定username的user数据，删除成功true，删除失败false-----------------------------------------------【通过username删除user数据】
 userDao.deleteByName = function (username, cb){
 	var sql = 'delete from	User where name = ?';
 	var args = [username];
@@ -239,9 +257,11 @@ userDao.deleteByName = function (username, cb){
 		if(err !== null){
 				utils.invokeCallback(cb,err.message, null);
 		} else {
+			//如果删除成功，数据库返回被删除对象，cb为true
 			if (!!res && res.affectedRows>0) {
 				utils.invokeCallback(cb,null,true);
 			} else {
+			//如果删除失败，数据库返回null，cb为false
 				utils.invokeCallback(cb,null,false);
 			}
 		}
@@ -257,7 +277,9 @@ userDao.deleteByName = function (username, cb){
  * @param {function} cb Call back function.
  */
 
-//生成一份用户数据（如果没有user表，会自动生成表，第一个用户id为1），通过用户数据实例一个用户，成功返回用户实例user
+//先从数据库生成user数据，然后通过返回的数据生成user实例-------------------------------------------------------【生成user实例】
+//（如果没有user表，会自动生成表，第一个用户id为1）
+//innsert插入数据，返回是数据对象，不是数组
 userDao.createUser = function (username, password, from, cb){
 	var sql = 'insert into User (name,password,`from`,loginCount,lastLoginTime) values(?,?,?,?,?)';
 	var loginTime = Date.now();
@@ -266,7 +288,7 @@ userDao.createUser = function (username, password, from, cb){
 		if(err !== null){
 			utils.invokeCallback(cb, {code: err.number, msg: err.message}, null);
 		} else {
-			//用户信息全部属性
+			//通过数据库返回对象，生成user实例
 			var user = new User({id: res.insertId, name: username, password: password, loginCount: 1, lastLoginTime:loginTime});
 			utils.invokeCallback(cb, null, user);
 		}
@@ -282,16 +304,17 @@ userDao.createUser = function (username, password, from, cb){
  * @param {function} cb Callback function
  */
 
-//创建一份角色数据（如果没有player表，会自动生成表，第一个角色id为1），通过角色数据生成player实体，成功返回player实体
+//先数据库生成player数据，然后通过返回的数据对象生成player实体----------------------------------------------------【生成player实体】
+//（如果没有player表，会自动生成表，第一个角色id为1）
 userDao.createPlayer = function (uid, name, roleId,cb){
 	var sql = 'insert into Player (userId, kindId, kindName, name, country, rank, level, experience, attackValue, defenceValue, hitRate, dodgeRate, walkSpeed, attackSpeed, hp, mp, maxHp, maxMp, areaId, x, y, skillPoint) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)';
 	//var role = dataApi.role.findById(roleId);
-	var character = dataApi.character.findById(roleId);
-  var role = {name: character.englishName, career: 'warrior', country: 1, gender: 'male'}
-	var born = consts.BornPlace;
-	var x = born.x + Math.floor(Math.random()*born.width);
+	var character = dataApi.character.findById(roleId);                                //角色数据（目前表中没有玩家的）
+  var role = {name: character.englishName, career: 'warrior', country: 1, gender: 'male'}  //角色信息
+	var born = consts.BornPlace;                                                       //出生地
+	var x = born.x + Math.floor(Math.random()*born.width);                             //生成出生点
 	var y = born.y + Math.floor(Math.random()*born.height);
-	var areaId = consts.PLAYER.initAreaId;
+	var areaId = consts.PLAYER.initAreaId;                                             //角色出生场景id
 	//role.country = 1;
 	var args = [uid, roleId, character.englishName, name, 1, 1, 1, 0, character.attackValue, character.defenceValue, character.hitRate, character.dodgeRate, character.walkSpeed, character.attackSpeed, character.hp, character.mp, character.hp, character.mp, areaId, x, y, 1];
 
@@ -301,7 +324,7 @@ userDao.createPlayer = function (uid, name, roleId,cb){
 			logger.error(err);
 			utils.invokeCallback(cb,err.message, null);
 		} else {
-			//玩家信息全部属性
+			//生成player实体
 			var player = new Player({
 				id: res.insertId,
 				userId: uid,
@@ -334,7 +357,7 @@ userDao.createPlayer = function (uid, name, roleId,cb){
  * @param {function} cb Callback function.
  */
 
-//通过角色数据，更新角色库中的数据。更新成功返回true，更新失败返回false
+//通过playerid，更新player数据。更新成功返回true，更新失败返回false-------------------------------------------【更新指定playerId的player数据】
 userDao.updatePlayer = function (player, cb){
 	var sql = 'update Player set x = ? ,y = ? , hp = ?, mp = ? , maxHp = ?, maxMp = ?, country = ?, rank = ?, level = ?, experience = ?, areaId = ?, attackValue = ?, defenceValue = ?, walkSpeed = ?, attackSpeed = ? , skillPoint = ? where id = ?';
 	var args = [player.x, player.y, player.hp, player.mp, player.maxHp, player.maxMp, player.country, player.rank, player.level, player.experience, player.areaId, player.attackValue, player.defenceValue, player.walkSpeed, player.attackSpeed, player.skillPoint, player.id];
@@ -343,9 +366,11 @@ userDao.updatePlayer = function (player, cb){
 		if(err !== null){
 			utils.invokeCallback(cb,err.message, null);
 		} else {
+			//如果更新成功，数据库返回数据对象，cb为true
 			if (!!res && res.affectedRows>0) {
 				utils.invokeCallback(cb,null,true);
 			} else {
+			//如果更新失败，数据库返回null，cb为false
 				logger.error('update player failed!');
 				utils.invokeCallback(cb,null,false);
 			}
@@ -360,7 +385,7 @@ userDao.updatePlayer = function (player, cb){
  * @param {function} cb Callback function.
  */
 
-//通过playerId，删除角色数据库中的数据，删除成功返回true，删除失败返回false
+//删除指定playerId的player数据，删除成功返回true，删除失败返回false
 userDao.deletePlayer = function (playerId, cb){
 	var sql = 'delete from	Player where id = ?';
 	var args = [playerId];
@@ -368,9 +393,11 @@ userDao.deletePlayer = function (playerId, cb){
 		if(err !== null){
 			utils.invokeCallback(cb,err.message, null);
 		} else {
+			//如果数据库返回player对象，cb为true
 			if (!!res && res.affectedRows>0) {
 				utils.invokeCallback(cb,null,true);
 			} else {
+			//如果数据库返回null，cb为false
 				utils.invokeCallback(cb,null,false);
 			}
 		}
