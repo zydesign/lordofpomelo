e0089b48e825d3dcf2095a6dcec3d8bd59b10935
zydesign <zydesign@126.com>
2018-01-28 11:37:59 +0800

    Update objectPoolManager.js

--- web-server/public/js/utils/pool/objectPoolManager.js
+++ web-server/public/js/utils/pool/objectPoolManager.js
@@ -4,16 +4,19 @@ __resources__["/objectPoolManager.js"] = {meta: {mimetype: "application/javascri
 	 * Module dependencies
 	 */
 		
+	//对象池管理。用于储存对象池
 	var ObjectPoolManager = function() {
-		this.pools = {};
+		this.pools = {};     //对象池 组
 	};
 
 	//Add pool named name to pools
+	//添加对象池
 	ObjectPoolManager.prototype.addPool = function(name, pool) {
 		this.pools[name] = pool;
 	};
 
 	//get pool named name from pools
+	//获取对象池
 	ObjectPoolManager.prototype.getPool = function(name) {
 		return this.pools[name];
 	};
