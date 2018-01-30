80502da40fa3d8d5a5b99339c2c9766778cf60cc
zydesign <zydesign@126.com>
2018-01-31 00:07:08 +0800

    Update taskReward.js

--- game-server/app/domain/taskReward.js
+++ game-server/app/domain/taskReward.js
@@ -20,37 +20,40 @@ var taskReward = module.exports;
  * @param {Array} ids
  * @api public
  */
+
+//奖赏。参数ids为完成未发送状态的task.id数组
 taskReward.reward = function(area, player, ids) {
 	if (ids.length < 1) {
 		return;
 	}
 
 	var i, l;
-	var tasks = player.curTasks;
-	var pos = player.getState();  //基类entity的函数，获取角色实体坐标
-	var totalItems = [], totalExp = 0;
+	var tasks = player.curTasks;           //玩家的已接任务
+	var pos = player.getState();           //基类entity的函数，获取角色实体坐标
+	var totalItems = [], totalExp = 0;     //奖赏的总装备物品（这里的道具id都是装备种类id）和总经验
 
+	//遍历已完成任务id组，获取累计到总装备物品totalItems和总经验totalExp中-----------------------------累计奖赏（总装备物品id、总经验）
 	for (i = 0, l=ids.length; i < l; i++) {
 		var id = ids[i];
 		var task = tasks[id];
 		
 		//split() 方法用于把一个字符串分割成字符串数组
 		//根据分号分割字符串为两段（如："1;25"变成["1","25"]）
-		var items = task.item.split(';'); 
-		var exp = task.exp;
-		//遍历数组，totalItems储存道具id组
+		var items = task.item.split(';');   //装备物品id数组（是kindId）
+		var exp = task.exp;                 //奖赏的经验
+		//遍历道具id数组，将道具id存入totalItems数组
 		for (var j = 0; j < items.length; j++) {
 			totalItems.push(items[j]);
 		}
 		totalExp += exp;
 	}
 
-	//奖励的装备数组
+	//通过总装备物品id），生成equipment实体组
 	var equipments = this._rewardItem(totalItems, pos);
 	//执行角色获得经验值
 	this._rewardExp(player, totalExp);
 
-	//实体装备加入场景
+	//实体装备加入场景（这游戏设定的奖励道具是掉落场景中）-------------------------------------让装备物品掉落场景中
 	for (i = 0, l=equipments.length; i < l; i ++) {
 		area.addEntity(equipments[i]);
 	}
@@ -67,7 +70,7 @@ taskReward.reward = function(area, player, ids) {
  * @return {Object}
  * @api private
  */
-//奖励道具，这里主要是装备，返回装备数组
+//奖励装备物品，返回equipment实体组
 taskReward._rewardItem = function(items, pos) {
 	var length = items.length;
 	var equipments = [];
@@ -76,7 +79,7 @@ taskReward._rewardItem = function(items, pos) {
 			var itemId = items[i];
 			//通过id获取装备数据
 			var itemData = dataApi.equipment.findById(itemId);
-			//生成装备实体
+			//生成装备物品 实体
 			var equipment = new Equipment({
 				kindId: itemData.id,
 				x: pos.x + Math.random() * 50,
@@ -106,6 +109,7 @@ taskReward._rewardItem = function(items, pos) {
  * @param {Number} exprience
  * @api private
  */
+//奖赏经验
 taskReward._rewardExp = function(player, exprience) {
 	player.addExperience(exprience);
 };
